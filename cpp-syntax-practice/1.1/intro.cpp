32c2be691035dea159ca503d1b293d766376c2f9
akshithere <130065975+akshithere@users.noreply.github.com>
2024-05-28 04:02:32 +0530

    Revisiting the basics of cpp like datatypes

---
+++ cpp-syntax-practice/1.1/intro.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // 1.1.1 Basic I/O -> cout & cin
+    // 1.1.2 DataTypes
+    int a = 2147483647;
+    //The size of int is arount -2 Billion to 2 Billion
+    // -2147483648 to +2147483647, memoryStorage is 4 bytes
+    cout << a << "\n";
+    /* To store more integers
+    long long int -> -(2^63) to (2^63)-1
+
+unsigned long long int -> 0 to 18,446,744,073,709,551,615
+    */
+
+//    Note: single long has minimal and no affect no compilers where range of int = range of long
+//  However for 16 bit compilers using long could be beneficial so single long isnt useless
+
+// For decimal numbes -> 
+// float, double, long double
+
+   // For char
+    char signedChar = 65; // 'A' in ASCII
+    cout << "Signed Char: " << signedChar << endl;
+
+    unsigned char unsignedChar = 97; // 'a' in ASCII
+    cout << "Unsigned Char: " << unsignedChar << endl;
+    return 0;
+}
